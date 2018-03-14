d247715efffe9184b226de90ce7f3b2ba7dd9159
androidbyvinti <32050504+androidbyvinti@users.noreply.github.com>
2018-03-14 16:47:29 +0530

    Add files via upload

---
+++ 12 March- Class Code/gridview/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.bmpl.gridview
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Glide.with(this)
+                .load("http://via.placeholder.com/300.png")
+                .into(imageView)
+    }
+}
